2cccc2c88cdb1330b8765365aaa977b5d24b8bff
Raymond Nhan <nhanraymond@gmail.com>
2023-06-19 20:41:24 -0700

    initialized React routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom/cjs/react-router-dom.min";
+
+import Home from "./routes/Home";
+import UpdatePage from "./routes/UpdatePage";
+import RestaurantDetailPage from "./routes/RestaurantDetailPage";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/restaurants/:id/update" component={UpdatePage} />
+          <Route
+            exact
+            path="/restaurants/:id"
+            component={RestaurantDetailPage}
+          />
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;

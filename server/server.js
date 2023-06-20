6b4c25583a2b7283c488b91551d47d94a91f88a8
Raymond Nhan <nhanraymond@gmail.com>
2023-06-19 17:19:55 -0700

    debug Pool syntax error in db/index & added restaurants route

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 require("dotenv").config();
 
 const express = require("express");
+const db = require("./db");
 const morgan = require("morgan");
 const app = express();
 
+app.use(express.json());
 app.use(morgan("dev"));
 
 const port = process.env.PORT || 3001;
+
+app.get("/api/v1/restaurants", async (req, res) => {
+  const results = await db.query(`
+  select * from restaurants
+  `);
+  res.status(200).json({
+    status: "success",
+    data: {
+      restaurant: [],
+    },
+  });
+});
 
 app.listen(port, () => {
   console.log(`server is up, listening on port ${port}`);

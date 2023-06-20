9322b705e21a40ca72832ebf90fb53ad8555e6c8
Raymond Nhan <nhanraymond@gmail.com>
2023-06-20 00:06:23 -0700

    + AddRestaurant component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,12 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom/cjs/react-router-dom.min";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import UpdatePage from "./routes/UpdatePage";
 import RestaurantDetailPage from "./routes/RestaurantDetailPage";
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
---
+++ client/src/components/AddRestaurant.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function AddRestaurant() {
+  return (
+    <div className="mb-4">
+      <form action="">
+        <div className="row">
+          <div className="col">
+            <input type="text" className="form-control" placeholder="name" />
+          </div>
+          <div className="col">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="location"
+            />
+          </div>
+          <div className="col">
+            <select className="form-control">
+              <option disabled>Price Range</option>
+              <option value="1">$</option>
+              <option value="2">$$</option>
+              <option value="3">$$$</option>
+              <option value="4">$$$$</option>
+              <option value="5">$$$$$</option>
+            </select>
+          </div>
+          <div className="col">
+            <button type="submit" className="btn btn-primary">
+              Add
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default AddRestaurant;

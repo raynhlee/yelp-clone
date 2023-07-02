e20295894f8052afdc901be275fc416a50425b56
Raymond Nhan <nhanraymond@gmail.com>
2023-07-02 13:19:56 -0700

    + states for AddRestaurant

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import RestaurantFinder from "../apis/RestaurantFinder";
+import { RestaurantsContext } from "../context/RestaurantsContext";
 
-function AddRestaurant() {
+const AddRestaurant = () => {
+  const { addRestaurants } = useContext(RestaurantsContext);
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [priceRange, setPriceRange] = useState("Price Range");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await RestaurantFinder.post("/", {
+        name,
+        location,
+        price_range: priceRange,
+      });
+      console.log(response.data.data);
+      addRestaurants(response.data.data.restaurant);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="mb-4">
       <form action="">
-        <div className="row">
+        <div className="form-row">
           <div className="col">
-            <input type="text" className="form-control" placeholder="name" />
+            <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              className="form-control"
+              placeholder="name"
+            />
           </div>
           <div className="col">
             <input
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
               className="form-control"
               type="text"
               placeholder="location"
             />
           </div>
           <div className="col">
-            <select className="form-control">
+            <select
+              value={priceRange}
+              onChange={(e) => setPriceRange(e.target.value)}
+              className="custom-select my-1 mr-sm-2"
+            >
               <option disabled>Price Range</option>
               <option value="1">$</option>
               <option value="2">$$</option>
@@ -25,15 +58,17 @@ function AddRestaurant() {
               <option value="5">$$$$$</option>
             </select>
           </div>
-          <div className="col">
-            <button type="submit" className="btn btn-primary">
-              Add
-            </button>
-          </div>
+          <button
+            onClick={handleSubmit}
+            type="submit"
+            className="btn btn-primary"
+          >
+            Add
+          </button>
         </div>
       </form>
     </div>
   );
-}
+};
 
 export default AddRestaurant;
